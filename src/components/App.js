b810f669735bcee95ea125b18e112391ce391a4b
programer10 <programer10@ucione.local>
2023-04-13 19:06:49 +0200

    randomstring implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import logo from '../assets/images/logo.svg';
 import '../assets/styles/App.css';
 
+import randomstring from 'randomstring';
+
 function App() {
+
+var rndStr = randomstring.generate();
+console.log("Random string = " + rndStr);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Moja prva aplikacija
+          Moja prva aplikacija, {rndStr}
         </p>
         <a
           className="App-link"
