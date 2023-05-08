63726f8446b86ebdab691730b75bc953946a9b32
programer10 <programer10@ucione.local>
2023-05-08 18:18:20 +0200

    Higher order conponent (HOC) and context tasks

---
+++ src/components/AppHOC.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const withColor = Element => ({color, ...otherProps}) => (
+    <Element {...otherProps} style={{backgroundColor: color}}/>
+);
+
+const Button = props => {
+    return <button {...props} />
+};
+
+const ColoredButton = withColor(Button);
+
+export default function AppHOC(){
+    return(
+        <div className="App">
+            <h1>Hello</h1>
+            <ColoredButton color="red">
+                Ja sam crven!
+            </ColoredButton>
+            <ColoredButton color="yellow">
+                Ja sam žut!
+            </ColoredButton>
+            <ColoredButton color="green">
+                Ja sam zelen!
+            </ColoredButton>
+        </div>
+    );
+}
