b2a09e95b582a172913de912632c684af0c3eec4
programer10 <programer10@ucione.local>
2023-05-11 17:34:52 +0200

    React Routes, zadatak 5.18.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App';
 import AppContext from './components/AppContext';
 import AppHOC from './components/AppHOC';
+import AppRouter from './components/AppRouter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppHOC />);
+root.render(<AppRouter />);
