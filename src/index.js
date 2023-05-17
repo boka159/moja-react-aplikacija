34d6ddb6c6cf58ff5f0f8f25cea659ccb66c81ef
programer10 <programer10@ucione.local>
2023-05-17 19:23:43 +0200

    zadatak 5.22, redux

--- src/index.js
+++ src/index.js
@@ -7,7 +7,17 @@ import AppHOC from './components/AppHOC';
 import AppRouter from './components/AppRouter';
 import AppAPI from './components/AppAPI';
 import AppGraphQL from './components/AppGraphQL';
+import AppRedux from './components/AppRedux';
 
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppGraphQL />);
+const store = createStore(reducer);
+
+root.render(
+<Provider store={store}>
+    <AppRedux />
+</Provider>
+);
